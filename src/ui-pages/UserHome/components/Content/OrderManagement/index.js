0790b18b476cb83ea6d9e5a89bfaa19110069fa6
jyotisman <jyotisman1984@gmail.com>
2021-02-24 15:30:30 +0530

    order manage 1st commit

--- src/ui-pages/UserHome/components/Content/OrderManagement/index.js
+++ src/ui-pages/UserHome/components/Content/OrderManagement/index.js
@@ -1,25 +1,48 @@
 import React from "react";
-import { Grid, withStyles }  from '@material-ui/core';
+import { Grid, withStyles, Paper }  from '@material-ui/core';
 import { connect } from "react-redux";
 import { mapDispatchToProps } from '../../../../../ui-utils/commons';
 
 const styles = theme => ({
-  
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  },
   });
 
-
 class OrderManagement extends React.Component{
-      
         render(){
          
             return(
-            <Grid container >
-            <Grid item xs={12}>
-            OrderManagement test
-            </Grid>
-          
-               
-            </Grid>
+              <div className={styles.root}>
+              <Grid container spacing={3}>
+                <Grid item xs={12}>
+                  <Paper className={styles.paper}>Order Management</Paper>
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                  <Paper className={styles.paper}>xs=12 sm=6</Paper>
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                  <Paper className={styles.paper}>xs=12 sm=6</Paper>
+                </Grid>
+                <Grid item xs={6} sm={3}>
+                  <Paper className={styles.paper}>xs=6 sm=3</Paper>
+                </Grid>
+                <Grid item xs={6} sm={3}>
+                  <Paper className={styles.paper}>xs=6 sm=3</Paper>
+                </Grid>
+                <Grid item xs={6} sm={3}>
+                  <Paper className={styles.paper}>xs=6 sm=3</Paper>
+                </Grid>
+                <Grid item xs={6} sm={3}>
+                  <Paper className={styles.paper}>xs=6 sm=3</Paper>
+                </Grid>
+              </Grid>
+            </div> 
            
             )
         }
